2cdee7839a9fd36bddea1f9ee2f61bba2f82a78f
Lengereh777 <amanuelkebede555@gmail.com>
2025-11-27 15:40:34 +0100

    -frisr

---
+++ netflix-clone/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Netflix Clone 2025!</h1>
+      <h2>Your ultimate destination for streaming movies and TV shows.</h2>
+      <h3>Explore our vast library and enjoy unlimited entertainment.</h3>
+      <h4>Stay tuned for exciting features coming soon.</h4>
+    </div>
+  );
+}
+
+export default App;
